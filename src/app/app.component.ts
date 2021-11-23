0bea502dab7c7063c874edd150a1e535059115fb
anurak18844 <anurak18844@gmail.com>
2021-11-23 16:53:03 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1: number = 5;
+  n2: number = 5;
+  picName = "https://images.pexels.com/photos/4588065/pexels-photo-4588065.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  hi(name:string){
+    alert('hi '+name);
+  }
 }
